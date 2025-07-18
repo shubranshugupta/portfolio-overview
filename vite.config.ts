4c795adef5333ac0332e5a18ca6d61ba6558fdf9
shubranshugupta <shubranshugupta@gmail.com>
2025-07-18 14:51:28 +0530

    configure build

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,16 @@ import react from '@vitejs/plugin-react';
 import federation from '@originjs/vite-plugin-federation';
 
 export default defineConfig({
+  build: {
+    target: 'esnext', // or 'modules'
+  },
   plugins: [
     react(),
     federation({
       name: 'my-mfe',
       filename: 'remoteEntry.js',
       exposes: {
-        './ExposedComponent': './src/components/ExposedComponent.tsx',
+        './App': './src/App.tsx',
       },
       shared: ['react', 'react-dom', '@mui/material'],
     }),
