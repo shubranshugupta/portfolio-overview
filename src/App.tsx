14dc58151a3225a83cc40a4b65a59fd87fbb8035
shubranshugupta <shubranshugupta@gmail.com>
2025-07-28 16:47:46 +0530

    Refactors risk chart, adds asset history view
    Modularizes the risk chart component structure and introduces an option to visualize the top 3 asset price histories alongside sector and asset exposures. Improves code organization by moving chart logic to a dedicated subfolder and enhances flexibility of exposure analysis for improved portfolio insights.

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { Brightness4Rounded, Brightness7Rounded } from '@mui/icons-material';
 
 import { enrichedHoldings } from './data/mockData';
 import PortfolioHoldingsTable from './components/holdingTable/PortfolioHoldingTable';
-import RiskChart from './components/RiskChart';
+import RiskChart from './components/risk/RiskChart';
 import { useThemeMode } from './components/context/ThemeModeContext';
 import PnLSummary from './components/pnl/PnLSummary';
 import { PortfolioProvider } from './components/context/PortfolioContext';
@@ -56,10 +56,10 @@ function App() {
                             <Grid size={{ xs: 12 }}>
                                 <PortfolioHoldingsTable holdings={enrichedHoldings} />
                             </Grid>
-                            <Grid size={{ xs: 12, md: 6 }}>
+                            <Grid size={{ xs: 12, md: 5 }}>
                                 <PnLSummary holdings={enrichedHoldings} />
                             </Grid>
-                            <Grid size={{ xs: 12, md: 6 }}>
+                            <Grid size={{ xs: 12, md: 7 }}>
                                 <RiskChart holdings={enrichedHoldings} />
                             </Grid>
                         </Grid>
